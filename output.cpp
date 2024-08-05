dcfad89a9101c4bcc6c114142d1dd1934706137b
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-08-05 11:33:26 +0530

    Add files via upload

---
+++ output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[4][2];
+    cout<<"enter the elements: ";
+     for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+        cin>>arr[i][j];
+        }
+    }
+   for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+        cout<<arr[i][j]<<" ";
+        }
+     cout<<endl;
+    }
+    }
