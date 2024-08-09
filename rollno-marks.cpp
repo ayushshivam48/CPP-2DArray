aaf79a7ff9e8de76014ee0faea325af7725e1a32
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-08-09 09:41:32 +0530

    Add files via upload

---
+++ rollno-marks.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[4][2]={{01,98},{02,78},{03,99},{04,69}};
+     for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+        cout<<arr[i][j]<<" ";
+        }
+     cout<<endl;
+    }
+}
